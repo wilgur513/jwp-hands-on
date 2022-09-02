ce6cd467344086b22f19cef7aef8b3b5a79ffa73
정진혁 <wilgur513@naver.com>
2022-09-02 20:59:48 +0900

    캐싱 적용

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,25 @@
 package com.example.etag;
 
+import com.example.version.ResourceVersion;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private final ResourceVersion version;
+
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> bean = new FilterRegistrationBean<>(
+                new ShallowEtagHeaderFilter());
+        bean.addUrlPatterns("/etag", "/resources/" + version.getVersion() + "/js/index.js");
+        return bean;
+    }
 }
